4678d57b206888bd0d493cf3c8417065447da49c
Aleksandr Fedorov <faost@mail.ru>
2012-04-26 17:45:57 +0400

    Add simple search form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,16 @@ import views.html.*;
 public class Application extends Controller {
 
   public static Result index() {
-    return ok(index.render(Category.allFront(), Event.all(), form(Search.class)));
+    return ok(index.render(Event.all(), form(Search.class)));
+  }
+  
+  public static Result search() {
+    Form<Search> searchForm = form(Search.class).bindFromRequest();
+    if (searchForm.hasErrors()) {
+      return badRequest(search.render(Event.all(), searchForm));
+    }
+    Search s = searchForm.get();
+    return ok(search.render(Event.all(), searchForm));
   }
   
   /**
--- app/models/Search.java
+++ app/models/Search.java
@@ -1,6 +1,29 @@
 package models;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import play.data.format.Formats;
+import play.data.validation.*;
+
 public class Search {
   
+  public String people;
+  
+  @Formats.DateTime(pattern="dd.mm.YYYY")
+  public Date date;
+  
+  @Constraints.Min(value=0)
+  @Constraints.Max(value=6)
   public Integer activity;
+  
+  public static Map<String, String> options() {
+    LinkedHashMap<String, String> options = new LinkedHashMap<String, String>();
+    
+    options.put("1", "Один");
+    options.put("2", "С друзьями");
+    
+    return options;
+  }
 }
